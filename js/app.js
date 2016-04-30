7bf54bd87df4e52dc18c2eae966738bade28fe5c
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-29 20:03:58 -0700

    Added animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngMessages'])
+angular.module('myApp', ['ngMessages', 'ngAnimate'])
   .controller('myCtrl', function($scope){
     var setValues = function(){
       $scope.inputs = {
